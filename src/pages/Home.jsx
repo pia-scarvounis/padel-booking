0d4bc463f576b96ff2e6dbab809b4580c9792d11
Pia Christina Scarvounis <pia.scarvounis@gmail.com>
2025-02-17 12:23:13 +0100

    laget en pages-mappe, enkel home-side med bakgrunnsbilde + enkel css og login-knapp

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className="home-container">
+            <h1>Velkommen til Padel Booking</h1>
+            <p>Her kan du booke en bane, se dine bookinger eller administrere systemet.</p>
+            <button>Logg inn</button>
+        </div>
+    );
+}
+
+export default Home;
